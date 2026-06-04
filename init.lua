8642f273405ecdeb9cfb56c71c025c16abdfcd57
Alexis Leclerc <web@alxs.fr>
2026-06-04 22:02:54 +0200

    Add color for @@> in markdown

--- init.lua
+++ init.lua
@@ -271,6 +271,14 @@ vim.api.nvim_create_autocmd('BufReadPost', {
   end,
 })
 
+-- Highlight whole lines containing the '@@>' marker
+vim.api.nvim_set_hl(0, 'ArrowMarker', { fg = '#F7768E', bg = '#392835', bold = true })
+vim.api.nvim_create_autocmd({ 'BufWinEnter', 'WinNew' }, {
+  desc = "Highlight lines containing '@@>'",
+  group = vim.api.nvim_create_augroup('arrow-marker', { clear = true }),
+  callback = function() vim.fn.matchadd('ArrowMarker', '.*@@>.*') end,
+})
+
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
